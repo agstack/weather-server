068900fc9e47fd2d0cbbf02a7cb159a775c28eb6
Ted Dunning <ted.dunning@gmail.com>
2021-08-29 17:59:52 -0700

    Lint fixes

--- pipeline/test_mrms_inventory.py
+++ pipeline/test_mrms_inventory.py
@@ -1,43 +1,46 @@
-import unittest
 from mrms_inventory import *
 import datetime
-import pandas
 import re
 import os.path
 import pytz
 
+
 def test_one_day():
-    inv = inventory(0, date(2021,8,21))
+    inv = inventory(0, datetime.date(2021, 8, 21))
     assert inv.shape[0] == 24
     assert inv.shape[1] == 3
-    verify_day(datetime.datetime(2021,8,21), inv, True)
+    verify_day(datetime.datetime(2021, 8, 21), inv, True)
+
 
 def test_start_offset():
-    for start in [-3, timedelta(days=-3), date(2021,8,18)]:
-        inv = inventory(start, date(2021,8,21))
+    for start in [-3, datetime.timedelta(days=-3), datetime.date(2021, 8, 18)]:
+        inv = inventory(start, datetime.date(2021, 8, 21))
         assert inv.shape[0] == 4 * 24
         assert inv.shape[1] == 3
         for i in range(0, 4):
             verify_day(datetime.datetime(2021, 8, 21+i-3), inv.iloc[i*24:(i+1)*24], True)
 
+
 def test_no_pattern():
-    inv = inventory(0, date(2021,8,21), mtime_pattern="", size_pattern="")  
+    inv = mrms_inventory.inventory(0, datetime.date(2021, 8, 21), mtime_pattern="", size_pattern="")
     assert inv.shape[0] == 24
     assert inv.shape[1] == 3
     verify_day(None, inv, False)
 
-    inv = inventory(0, date(2021,8,21), size_pattern="")  
+    inv = mrms_inventory.inventory(0, datetime.date(2021, 8, 21), size_pattern="")
     assert inv.shape[0] == 24
     assert inv.shape[1] == 3
-    verify_day(datetime.datetime(2021,8,21), inv, False)
+    verify_day(datetime.datetime(2021, 8, 21), inv, False)
+
 
 def test_invalid():
     try:
-        inv = inventory(0, date(2021,8,21), mtime_pattern="")
+        inv = mrms_inventory.inventory(0, datetime.date(2021, 8, 21), mtime_pattern="")
         assert not "Expected exception"
     except ValueError:
         pass
-        
+
+
 def verify_day(expected, inv, has_mtime=True):
     for f in inv["url"]:
         assert re.match(r"Multi.*gz", os.path.basename(f))
@@ -55,4 +58,3 @@ def verify_day(expected, inv, has_mtime=True):
             actual = tz.localize(datetime.datetime.strptime(d, "%Y-%m-%d %H:%M")).astimezone(pytz.utc)
             delta = actual - pytz.utc.localize(expected)
             assert delta <= datetime.timedelta(hours=25)
-                
