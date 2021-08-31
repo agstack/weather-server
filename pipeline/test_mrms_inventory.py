7eda47c3565f3469bf74e90e9cab4e3bfc037876
Ted Dunning <ted.dunning@gmail.com>
2021-08-30 22:24:51 -0700

    Updated inventory and download and corresponding tests to handle date-nested directories by carrying date in the inventory

--- pipeline/test_mrms_inventory.py
+++ pipeline/test_mrms_inventory.py
@@ -8,7 +8,9 @@ import pytz
 def test_one_day():
     inv = mrms_inventory.inventory(0, datetime.date(2021, 8, 21))
     assert inv.shape[0] == 24
-    assert inv.shape[1] == 3
+    assert inv.shape[1] == 4
+    d = inv["date"][0]
+    print(f"{d} {type(d)} {d == datetime.date(2021, 8, 21)}")
     verify_day(datetime.datetime(2021, 8, 21), inv, True)
 
 
@@ -16,7 +18,7 @@ def test_start_offset():
     for start in [-3, datetime.timedelta(days=-3), datetime.date(2021, 8, 18)]:
         inv = mrms_inventory.inventory(start, datetime.date(2021, 8, 21))
         assert inv.shape[0] == 4 * 24
-        assert inv.shape[1] == 3
+        assert inv.shape[1] == 4
         for i in range(0, 4):
             verify_day(datetime.datetime(2021, 8, 21+i-3), inv.iloc[i*24:(i+1)*24], True)
 
@@ -24,12 +26,12 @@ def test_start_offset():
 def test_no_pattern():
     inv = mrms_inventory.inventory(0, datetime.date(2021, 8, 21), mtime_pattern="", size_pattern="")
     assert inv.shape[0] == 24
-    assert inv.shape[1] == 3
+    assert inv.shape[1] == 4
     verify_day(None, inv, False)
 
     inv = mrms_inventory.inventory(0, datetime.date(2021, 8, 21), size_pattern="")
     assert inv.shape[0] == 24
-    assert inv.shape[1] == 3
+    assert inv.shape[1] == 4
     verify_day(datetime.datetime(2021, 8, 21), inv, False)
 
 
@@ -51,6 +53,9 @@ def verify_day(expected, inv, has_mtime=True):
             assert s is None
 
     if expected:
+        for d in inv["date"]:
+            assert d == expected.date()
+
         for d in inv["mtime"]:
             # Iowa State is in Ames, Iowa which is the same timezone as Chicago
             tz = pytz.timezone("America/Chicago")
@@ -58,3 +63,4 @@ def verify_day(expected, inv, has_mtime=True):
             actual = tz.localize(datetime.datetime.strptime(d, "%Y-%m-%d %H:%M")).astimezone(pytz.utc)
             delta = actual - pytz.utc.localize(expected)
             assert delta <= datetime.timedelta(hours=25)
+
